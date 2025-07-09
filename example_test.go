31af8337f726c0e38c29587b3d3dd43557335304
Oleg Kovalov <hey@olegk.dev>
2025-07-09 12:14:55 +0200

    Add IsValidFormat

--- example_test.go
+++ example_test.go
@@ -6,6 +6,35 @@ import (
 	"github.com/cristalhq/junkemail"
 )
 
+func ExampleIsValidFormat() {
+	{
+		email := "drop@10-minute-mail.com"
+		isValid := junkemail.IsValidFormat(email)
+		fmt.Printf("is '%s' valid? - %v\n", email, isValid)
+	}
+	{
+		email := "drop@.com"
+		isValid := junkemail.IsValidFormat(email)
+		fmt.Printf("is '%s' valid? - %v\n", email, isValid)
+	}
+	{
+		email := "hehe"
+		isValid := junkemail.IsValidFormat(email)
+		fmt.Printf("is '%s' valid? - %v\n", email, isValid)
+	}
+	{
+		email := ""
+		isValid := junkemail.IsValidFormat(email)
+		fmt.Printf("is '%s' valid? - %v\n", email, isValid)
+	}
+
+	// Output:
+	// is 'drop@10-minute-mail.com' valid? - true
+	// is 'drop@.com' valid? - false
+	// is 'hehe' valid? - false
+	// is '' valid? - false
+}
+
 func ExampleCheck() {
 	email := "drop@10-minute-mail.com"
 
